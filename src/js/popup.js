80583e4add0c5225f8333b529147a2a090b0a355
jpictos <jpicatos@ucm.es>
2020-04-09 13:33:36 +0200

    popup languajes change

--- src/js/popup.js
+++ src/js/popup.js
@@ -3,7 +3,7 @@ function playPause() {
     chrome.tabs.query({
         currentWindow: true,
         active: true
-    }, function (tabs) {
+    }, function(tabs) {
         var activeTab = tabs[0];
         tab = activeTab;
         window.close();
@@ -13,6 +13,37 @@ function playPause() {
     });
 }
 
-document.addEventListener("DOMContentLoaded", function () {
+function isPause() {
+    let tab;
+    chrome.tabs.query({
+        currentWindow: true,
+        active: true
+    }, function(tabs) {
+        var activeTab = tabs[0];
+        tab = activeTab;
+        chrome.tabs.sendMessage(activeTab.id, {
+            "functiontoInvoke": "isPause"
+        }, handleResponse);
+
+        function handleResponse(isPause) {
+            if (isPause) {
+                document.getElementById("playPause").innerText = chrome.i18n.getMessage("playExtension");
+            } else {
+                document.getElementById("playPause").innerText = chrome.i18n.getMessage("pauseExtension");
+            }
+        }
+    });
+}
+document.addEventListener("DOMContentLoaded", function() {
+    isPause();
     document.getElementById("playPause").addEventListener("click", playPause);
+});
+
+chrome.runtime.onMessage.addListener(function(request, sender) {
+    if (request.options.pause) {
+        document.getElementById("playPause").innerText = chrome.i18n.getMessage("playExtension");
+    } else {
+        document.getElementById("playPause").innerText = chrome.i18n.getMessage("pauseExtension");
+    }
+
 });
