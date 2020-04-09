a8c2e8f3d9d55a2f9a12ab6c04d3705a37de4154
jpictos <jpicatos@ucm.es>
2020-04-09 13:31:59 +0200

    set badge text

--- src/js/background.js
+++ src/js/background.js
@@ -2,7 +2,7 @@ function getword(info, tab) {
     chrome.tabs.query({
         "active": true,
         "currentWindow": true
-    }, function (tabs) {
+    }, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {
             "functiontoInvoke": "holacaracola"
         });
@@ -18,18 +18,24 @@ chrome.contextMenus.create({
 chrome.runtime.onMessage.addListener(function(request, sender) {
     if (request.type == "changeIcon") {
         if (request.options.pause) {
-            chrome.browserAction.setIcon({path: "pause38.png"});
+            chrome.browserAction.setIcon({ path: "pause38.png" });
         } else {
-            chrome.browserAction.setIcon({path: "48.png"});
-        }   
+            chrome.browserAction.setIcon({ path: "48.png" });
+        }
+    }
+
+    if (request.type == "popupremoved") {
+        chrome.browserAction.setBadgeBackgroundColor({ color: "#333333" });
+        chrome.browserAction.setBadgeText({ text: "✓" });
     }
 });
 
 chrome.tabs.onActivated.addListener(function(activeInfo) {
+    chrome.browserAction.setBadgeText({ text: "" });
     chrome.tabs.query({
         "active": true,
         "currentWindow": true
-    }, function (tabs) {
+    }, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {
             "functiontoInvoke": "changeTab"
         });
